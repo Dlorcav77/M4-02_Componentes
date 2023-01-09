4b9a4b1cac3f0e5f7ca39b2c7f43bfb304aca8b4
Diego_Lorca <diego.lorcaveliz@gmail.com>
2023-01-09 18:48:26 -0300

    03

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import App from './App'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
